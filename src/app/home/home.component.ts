3d5a042ad9b39afac0c52392e7fb8476a59b54f7
David <39265971+somervda@users.noreply.github.com>
2023-06-28 10:32:45 -0400

    wip

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { HostService } from '../services/host.service';
+import { interval } from 'rxjs/internal/observable/interval';
+import { startWith, Subscription, switchMap } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,10 +10,20 @@ import { HostService } from '../services/host.service';
 })
 export class HomeComponent implements OnInit {
   hostStatus: any;
+  hostService$$: undefined | Subscription;
   constructor(private hostService: HostService) {}
 
   ngOnInit(): void {
     this.getHostStatus();
+    // Set up polling for sensors, check each 15 seconds
+    this.hostService$$ = interval(15000)
+      .pipe(
+        startWith(0),
+        switchMap(() => this.hostService.getHostStatus())
+      )
+      .subscribe((response) => {
+        this.hostStatus = response;
+      });
   }
 
   getHostStatus(): void {
