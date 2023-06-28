efc3f1480775772dc8103dc07f95d6173770dcdb
David <39265971+somervda@users.noreply.github.com>
2023-06-28 17:28:19 -0400

    Unsubscribed when leaving home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { ThisReceiver } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HostService } from '../services/host.service';
 import { interval } from 'rxjs/internal/observable/interval';
 import { startWith, Subscription, switchMap } from 'rxjs';
@@ -8,13 +8,12 @@ import { startWith, Subscription, switchMap } from 'rxjs';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   hostStatus: any;
   hostService$$: undefined | Subscription;
   constructor(private hostService: HostService) {}
 
   ngOnInit(): void {
-    this.getHostStatus();
     // Set up polling for sensors, check each 15 seconds
     this.hostService$$ = interval(15000)
       .pipe(
@@ -35,5 +34,12 @@ export class HomeComponent implements OnInit {
   iconClick() {
     this.hostStatus = [];
     this.getHostStatus();
+  }
+
+  ngOnDestroy(): void {
+    if (this.hostService$$) {
+      console.log('Close hostService$$');
+      this.hostService$$.unsubscribe();
+    }
   }
 }
